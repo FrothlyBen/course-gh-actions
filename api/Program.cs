4b357b22b053c6bde9586373df926b47f8f7cbf7
Ben Kinder <ben@frothly.org>
2025-05-08 18:48:56 -0400

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -18,6 +18,11 @@ var newSummaries = new[]
     "Brrrrr", "Yikes", "Frozen", "Hot", "Ouch", "StopIt"
 };
 
+var Summaries = new[]
+{
+    "Brrrrr", "Yikes", "Frozen", "Hot", "Ouch", "StopIt"
+};
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
